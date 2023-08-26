b96b2625d63f7fc1fe67f01c5cc26db2335e0fbb
fixedcow <puren37@gmail.com>
2023-08-26 22:27:33 +0900

    [Feat] : 튜토리얼 구성 중

--- W02_Assignment/Assets/Scripts/JW/Pillar.cs
+++ W02_Assignment/Assets/Scripts/JW/Pillar.cs
@@ -18,8 +18,8 @@ public class Pillar : MonoBehaviour
 	private void OnEnable()
 	{
 		transform.parent.TryGetComponent(out parent);
-		float yScale = Mathf.Clamp(parent.position.y, 1, float.MaxValue);
-		float yPos = Mathf.Clamp(-(yScale / 2) - 1f, float.MinValue, -0.5f);
+		float yScale = Mathf.Clamp(parent.position.y + 1, 1, float.MaxValue);
+		float yPos = Mathf.Clamp(-(yScale / 2), float.MinValue, 0.5f);
 		transform.localScale = new Vector3(transform.localScale.x, yScale, transform.localScale.z);
 		transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
 	}
