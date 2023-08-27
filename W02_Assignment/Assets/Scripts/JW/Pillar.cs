602ab7be504bd1a925c8151aba3645862634932e
fixedcow <puren37@gmail.com>
2023-08-27 13:27:17 +0900

    [Feat] : 튜토리얼 텍스트 추가
    이외에도 인디케이터 알파 블랜딩 임시로 해결...

--- W02_Assignment/Assets/Scripts/JW/Pillar.cs
+++ W02_Assignment/Assets/Scripts/JW/Pillar.cs
@@ -9,6 +9,7 @@ public class Pillar : MonoBehaviour
 
 	#region PrivateVariables
 	private Transform parent;
+	[SerializeField] private float yMod;
 	#endregion
 
 	#region PublicMethod
@@ -19,7 +20,7 @@ public class Pillar : MonoBehaviour
 	{
 		transform.parent.TryGetComponent(out parent);
 		float yScale = Mathf.Clamp(parent.position.y + 1, 1, float.MaxValue);
-		float yPos = Mathf.Clamp(-(yScale / 2), float.MinValue, 0.5f);
+		float yPos = Mathf.Clamp(-(yScale / 2) + yMod, float.MinValue, 0.5f);
 		transform.localScale = new Vector3(transform.localScale.x, yScale, transform.localScale.z);
 		transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
 	}
